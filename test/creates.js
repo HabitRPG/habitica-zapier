6fad0b1a9ab31c215a570d0260801d16e4a05d93
Majed <m.alsaeed@tamkeentech.sa>
2018-07-04 19:55:49 +0300

    * [Fix] Upgraded dependecies. * [Fix] Sample in find_task.js. * [New] Support nerdamer symbolic math expressions for task priority.

---
+++ test/creates.js
@@ -0,0 +1,31 @@
+require('should');
+
+const zapier = require('zapier-platform-core');
+
+const App = require('../index');
+const appTester = zapier.createAppTester(App);
+
+describe('creates', () => {
+
+  describe('create task create', () => {
+    it('should create a new task', (done) => {
+      const bundle = {
+        authData: {
+          userId: process.env.USER_ID,
+          apiKey: process.env.API_KEY
+        },
+        inputData: {
+          type: 'todo',
+          text: 'Write more tests :)',
+          priority: '1000/2000'
+        }
+      };
+      appTester(App.creates.create_task.operation.perform, bundle)
+        .then((result) => {
+          result.should.have.property('text');
+          done();
+        })
+        .catch(done);
+    });
+  });
+});
